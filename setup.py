c08a736a149c932bed5919ca9ad8ba68c95acdf8
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-29 15:55:28 +0900

    Add added icons to package_data section in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    package_data={'pyqt_svg_viewer.ico': ['svg.svg']},
+    package_data={'pyqt_svg_viewer.ico': ['add_dir.svg', 'add_file.svg', 'close.svg', 'full_screen.svg',
+                                          'navigation_bar.svg', 'svg.svg']},
     description='PyQt SVG viewer',
     url='https://github.com/yjg30737/pyqt-svg-viewer.git',
     install_requires=[
